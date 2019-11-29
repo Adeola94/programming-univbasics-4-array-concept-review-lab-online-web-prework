3213b146147ba15e159d9b419d020a29a666fb72
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-11-29 01:44:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -27,4 +27,9 @@ scale = [2,3,4,5]
   hill = [1,2,3,4,5,4,3,2,1]
   valley = [5,4,3,2,1,0,1,2,3,4,5]
   zig_zag = [500,4,1000,5,250]
+  find_min_value(scale).to eq(2)
+    find_min_value(scale_2).to eq(3)
+      find_min_value(hill).to eq(1)
+      find_min_value(valley).to eq(0)
+      find_min_value(zig_zag).to eq(4)
 end
