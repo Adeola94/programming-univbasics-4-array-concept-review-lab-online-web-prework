4ab080abd78175481954f2678b71084ff5026b47
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-11-29 01:42:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,7 +11,14 @@ scale = [2,3,4,5]
 end
 
 def find_max_value(array)
-  # Add your solution here
+  scale = [2,3,4,5]
+  scale_2 = [6,5,4,3]
+  hill = [1,2,3,4,5,4,3,2,1]
+  valley = [5,4,3,2,1,0,1,2,3,4,5]
+  zig_zag = [500,4,1000,5,250]
+  find_element_index(hill,500).to eq(nil)
+   find_element_index(valley,500).to eq(nil)
+  find_element_index(zig_zag,400).to eq(nil)
 end
 
 def find_min_value(array)
