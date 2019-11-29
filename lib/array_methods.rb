c16d990a030455f3c6616778070059cc33257771
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-11-29 01:40:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,7 +5,9 @@ scale = [2,3,4,5]
   valley = [5,4,3,2,1,0,1,2,3,4,5]
   zig_zag = [500,4,1000,5,250]
   find_element_index(scale,2).to eq(0)
-
+  find_element_index(scale,4)).to eq(2)
+   find_element_index(scale_2,5)).to eq(1)
+  find_element_index(scale_2,6)).to eq(0)
 end
 
 def find_max_value(array)
