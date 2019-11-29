3830c3f7c950608d882d1da60f2baf3ca7368750
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-11-29 01:44:16 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,35 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+scale = [2,3,4,5]
+  scale_2 = [6,5,4,3]
+  hill = [1,2,3,4,5,4,3,2,1]
+  valley = [5,4,3,2,1,0,1,2,3,4,5]
+  zig_zag = [500,4,1000,5,250]
+  find_element_index(scale,2).to eq(0)
+  find_element_index(scale,4)).to eq(2)
+   find_element_index(scale_2,5)).to eq(1)
+  find_element_index(scale_2,6)).to eq(0)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  scale = [2,3,4,5]
+  scale_2 = [6,5,4,3]
+  hill = [1,2,3,4,5,4,3,2,1]
+  valley = [5,4,3,2,1,0,1,2,3,4,5]
+  zig_zag = [500,4,1000,5,250]
+  find_element_index(hill,500).to eq(nil)
+   find_element_index(valley,500).to eq(nil)
+  find_element_index(zig_zag,400).to eq(nil)
 end
 
 def find_min_value(array)
-  # Add your solution here
+scale = [2,3,4,5]
+  scale_2 = [6,5,4,3]
+  hill = [1,2,3,4,5,4,3,2,1]
+  valley = [5,4,3,2,1,0,1,2,3,4,5]
+  zig_zag = [500,4,1000,5,250]
+  find_min_value(scale).to eq(2)
+    find_min_value(scale_2).to eq(3)
+      find_min_value(hill).to eq(1)
+      find_min_value(valley).to eq(0)
+      find_min_value(zig_zag).to eq(4)
 end
